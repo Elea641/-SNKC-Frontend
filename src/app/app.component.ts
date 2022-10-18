7f748fec79222fc9e5cbdc476d1b680609e5704e
MaraLfs <99500975+MaraLfs@users.noreply.github.com>
2022-10-18 11:16:38 +0200

    nettoyage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,19 +8,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-  constructor(private http: HttpClient) {
+  constructor() {
   }
  
 }
-
-// Exemple de post et get sur json server
-
-// const user: User = new User(3, '', '', '', '', '','', '', 0,'', new Date(), new Date());
-    // this.http.post('http://localhost:3000/users', user).subscribe(res => {
-    //   console.log(JSON.stringify(res));
-    // });
-
-    // this.http.get('http://localhost:3000/users?id=2').subscribe(res => {
-    //   console.log(JSON.stringify(res));
-    // })
-    // console.log(JSON.stringify(user));
