c56fa78050da7a6c65c42ed4cd8e299b9fa9e0e2
MaraLfs <99500975+MaraLfs@users.noreply.github.com>
2022-10-19 11:23:26 +0200

    creation composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CollectionComponent } from './collection/collection.component';
+import { AuctionsComponent } from './auctions/auctions.component';
+import { AuctionComponent } from './auction/auction.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { SneakersComponent } from './sneakers/sneakers.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactComponent,
+    LegalNoticeComponent,
+    ProfileComponent,
+    CollectionComponent,
+    AuctionsComponent,
+    AuctionComponent,
+    FavoriteComponent,
+    SneakersComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
