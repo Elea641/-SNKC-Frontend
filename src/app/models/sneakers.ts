d57115f4b4fe2de05894ac3283986216d434ccec
MaraLfs <99500975+MaraLfs@users.noreply.github.com>
2022-10-19 17:57:38 +0200

    modif follows sur model user

--- src/app/models/sneakers.ts
+++ src/app/models/sneakers.ts
@@ -2,6 +2,7 @@ import { StateOfWear } from "./enum/stateofwear";
 import { Colors } from "./enum/colors";
 import { User } from "./user";
 import { Picture } from "./picture";
+import { count } from "rxjs";
 
 export class Sneakers {
     constructor(
@@ -14,7 +15,7 @@ export class Sneakers {
         public updateDate: Date,
         public user: User,
         public pictures: Picture[],
-        public follows: User[],
+        public follows: number,
         public dateOfPurchase?: Date,
         public mainColor?: Colors,
         public authentification?: boolean,
