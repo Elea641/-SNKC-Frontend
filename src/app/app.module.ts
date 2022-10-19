805579b6add58c72b828eaa8ad7748f4a85edbab
ESCARMANT Adrien <92037454+E-Adrien8@users.noreply.github.com>
2022-10-19 11:44:25 +0200

    creation headerNav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavComponent
   ],
   imports: [
     BrowserModule
