7d8b9d53c30399b49a698466816e5dc625ba9f3f
MaraLfs <99500975+MaraLfs@users.noreply.github.com>
2022-11-03 11:36:40 +0100

    eslint

--- src/app/models/room.ts
+++ src/app/models/room.ts
@@ -1,15 +1,15 @@
-import { Sneakers } from "./sneakers";
-import { User } from "./user";
+import { Sneakers } from './sneakers';
+import { User } from './user';
 
 export class Room {
-    constructor(
-        public id: number,
-        public owner: User,
-        public sneakers: Sneakers,
-        public initialPrice: number,
-        public startDate: Date,
-        public endDate : Date,
-        public attendees: User[],
-        public winnerAuction?: User,
-    ) {}
+	constructor(
+		public id: number,
+		public owner: User,
+		public sneakers: Sneakers,
+		public initialPrice: number,
+		public startDate: Date,
+		public endDate: Date,
+		public attendees: User[],
+		public winnerAuction?: User
+	) {}
 }
