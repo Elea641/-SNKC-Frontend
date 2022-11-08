e3e34c769021d1806d31fa4ad71290ce455f3c0b
MaraLfs <99500975+MaraLfs@users.noreply.github.com>
2022-11-08 11:58:00 +0100

    corrections link back

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,36 +1,8 @@
-<<<<<<< HEAD
-import { Role } from "../interfaces/role";
-import { Sneakers } from "./sneakers";
-
-export class User {
-    public isAdmin!: boolean;
-    constructor(
-        public id: number, 
-        public firstname: string, 
-        public lastname: string, 
-        public username: string,
-        public email: string,
-        public password: string,
-        public sneakers: Sneakers[],
-        public sneakersFollowed: Sneakers[],
-        public usersFollowed: User[],
-        public shippingAddress: string,
-        public shippingCity: string,
-        public shippingZipCode: number,
-        public shippingCountry: string, 
-        public createDate: Date,
-        public updateDate: Date,
-        public roles: Role[],
-        public avatar?: string,
-        public billingAddress?: string,
-        public billingCity?: string,
-        public billingZipCode?: number,
-        public billingCountry?: string,
-    ) {}
-=======
+import { Role } from '../interfaces/role';
 import { Sneakers } from './sneakers';
 
 export class User {
+	public isAdmin!: boolean;
 	constructor(
 		public id: number,
 		public firstname: string,
@@ -47,11 +19,11 @@ export class User {
 		public shippingCountry: string,
 		public createDate: Date,
 		public updateDate: Date,
+		public roles: Role[],
 		public avatar?: string,
 		public billingAddress?: string,
 		public billingCity?: string,
 		public billingZipCode?: number,
 		public billingCountry?: string
 	) {}
->>>>>>> f56ec382bc9d0538dbff8162011ff6fd42e4a739
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,36 +1,39 @@
-
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import {
+	FormBuilder,
+	FormControl,
+	FormGroup,
+	Validators,
+} from '@angular/forms';
 import { AuthService } from '../services/auth.service';
-
+import { Router} from '@angular/router';
 
 @Component({
 	selector: 'app-register',
 	templateUrl: './register.component.html',
 	styleUrls: ['./register.component.css'],
 })
+export class RegisterComponent {
+	public form: FormGroup;
 
-export class RegisterComponent implements OnInit {
+	constructor(private router: Router, private fb: FormBuilder, private authService: AuthService) {
+		this.form = this.fb.group({
+			email: new FormControl('', [Validators.required]),
+			password: new FormControl('', [Validators.required]),
+			roles: new FormControl(['user']),
+		});
+	}
 
-  public form: FormGroup;
-
-  constructor(private fb: FormBuilder,
-    private authService: AuthService) { 
-    this.form = this.fb.group({
-      'email': new FormControl('', [Validators.required]),
-      'password': new FormControl('', [Validators.required]),
-      'roles': new FormControl(['user']),
-    });
-  }
-
-  ngOnInit(): void {
-  }
-
-  onRegisterSubmit(){
-    if (this.form.valid){
-      this.authService.register(this.form.getRawValue()).subscribe(
-        
-      )
-    }
-  }
+	onRegisterSubmit() {
+		if (this.form.valid) {
+			this.authService.register(this.form.getRawValue()).subscribe(
+				_ => {
+					this.router.navigate(['login']);
+				},
+				(error: Error) => {
+					console.log(error);
+				}
+			);
+		}
+	}
 }
