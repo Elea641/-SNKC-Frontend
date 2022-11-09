2bf01c257153a749972ce015971cbd5da97a1b48
elea641 <eleavoliotis64@gmail.com>
2022-11-09 11:30:18 +0100

    first commit

--- src/app/services/sneakers.service.ts
+++ src/app/services/sneakers.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Sneakers } from '../models/sneakers';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
 	providedIn: 'root'
@@ -14,19 +15,25 @@ export class SneakersService {
 	constructor(private http: HttpClient) { }
 
 	getSneakersById(id: string): Observable<Sneakers> {
-		return this.http.get<Sneakers>(`${this.baseUrl}/${id}`);
+		return this.http.get<Sneakers>(environment.urlApi + id);
 	}
 
 	getAllSneakersByUserId(id: string): Observable<Sneakers[]> {
-		return this.http.get<Sneakers[]>(`${this.baseUrl}?user.id=${id}`);
+		return this.http.get<Sneakers[]>(environment.urlApi + { 'id': sessionStorage.getItem('id')});
 	}
 
 	get(): Observable<Sneakers[]> {
-		return this.http.get<Sneakers[]>(`${this.baseUrl}`);
+		return this.http.get<Sneakers[]>(environment.urlApi);
 	}
 
 	public deleteSneakersById(id: string): void{
-		this.http.delete(`${this.baseUrl}/${id}`).subscribe();
+		this.http.delete(environment.urlApi + id).subscribe();
 	}
+
+// 	postSneakersCreated(id: string): Observable<Sneakers> {
+// 		return 	this.http.post(environment.urlApi, sneakers).subscribe(res => {
+// 			this.router.navigate(['/sneakers/:id']);
+
+// 	}
 }
 
