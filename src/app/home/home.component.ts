454d2d3c4afacaba53961b301651958515acd94a
ESCARMANT Adrien <92037454+E-Adrien8@users.noreply.github.com>
2022-11-18 14:08:39 +0100

    paralax

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
-
+import {Component, ElementRef, ViewChild, OnInit} from '@angular/core';
+import simpleParallax from 'simple-parallax-js';
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+	@ViewChild('paralax') paralaxRef!: ElementRef<HTMLDivElement>;
+	public image = document.querySelector('#paralax') as HTMLDivElement | null;
+	constructor() {
+		if(this.image != null) {
+			new simpleParallax(this.image, {
+				overflow: true
+			});
+		}
+	}
+	ngOnInit() {
+	};
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,50 +6,25 @@ import { User } from '../models/user';
 import { Role } from '../interfaces/role';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class UserService {
+	constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+	getUserById(id: string): Observable<User> {
+		return this.http.get<User>(environment.urlApi + id);
+	}
 
-  getUserById(id: string): Observable<User> {
-    return this.http.get<User>(environment.urlApi + id)
-  }
-
-  getConnectedUser(): Observable<User> {
-    return this.http.get<User>(environment.urlApi + 'users/me');
-  }
-
-		updateMe(user: object): Observable<User> {
+	getConnectedUser(): Observable<User> {
+		return this.http.get<User>(environment.urlApi + 'users/me').pipe(
+			map((user: User) => {
+				user.isAdmin = user.roles?.some((role: Role) => role.id === 1);
+				return user;
+			})
+		);
+	}
+	updateMe(user: object): Observable<User> {
 			return this.http.patch<User>(environment.urlApi + 'users/me', user);
 		}
-
 }
-// =======
-// import { environment } from './../../environments/environment';
-// import { Injectable } from '@angular/core';
-// import { map, Observable } from 'rxjs';
-// import { HttpClient } from '@angular/common/http';
-// import { User } from '../models/user';
-// import { Role } from '../interfaces/role';
-//
-// @Injectable({
-// 	providedIn: 'root',
-// })
-// export class UserService {
-// 	constructor(private http: HttpClient) {}
-//
-// 	getUserById(id: string): Observable<User> {
-// 		return this.http.get<User>(environment.urlApi + id);
-// 	}
-//
-// 	getConnectedUser(): Observable<User> {
-// 		return this.http.get<User>(environment.urlApi + 'users/me').pipe(
-// 			map((user: User) => {
-// 				user.isAdmin = user.roles?.some((role: Role) => role.id === 1);
-// 				return user;
-// 			})
-// 		);
-// 	}
-// }
-// >>>>>>> 3647b3b39859ca0d8fbce685f4ac3f616b047416
+
