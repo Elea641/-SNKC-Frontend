c60ab52902e7f79e08376f6fd71b0e41599615c8
ESCARMANT Adrien <92037454+E-Adrien8@users.noreply.github.com>
2022-11-21 09:42:44 +0100

    re

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, ViewChild, OnInit} from '@angular/core';
-import simpleParallax from 'simple-parallax-js';
+
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
